b3e28ed4a6d5f1c89d5f450f7327e56dab045da7
reyankhan0342 <sashaandoo0342@gmail.com>
2024-01-02 12:54:38 +0500

    Covid 19 Api Calling in this app complete application

---
+++ android/app/src/main/kotlin/com/example/covid_19_api_complete_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.covid_19_api_complete_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ViewModel/world_sates_view_model.dart
@@ -0,0 +1,36 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../Model/world_states.dart';
+import 'Utilities/app_url.dart';
+
+class WorldStatesViewModel {
+
+
+  Future<WorldStatesModel> fetchWorldRecords() async {
+    final response = await http.get(Uri.parse(AppUrl.worldStatesApi));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return WorldStatesModel.fromJson(data);
+    } else {
+      throw Exception('Error');
+    }
+  }
+
+  Future<List<dynamic>> countriesListApi() async {
+    var data ;
+    final response = await http.get(Uri.parse(AppUrl.countriesList));
+    print(response.statusCode.toString());
+    print(data);
+    if (response.statusCode == 200) {
+       data = jsonDecode(response.body.toString());
+      return data;
+    } else {
+      throw Exception('Error');
+    }
+  }
+
+}
